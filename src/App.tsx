e0d59dde4f0daa4981009dea05fae6f4100f99d0
Roberto Carvalho <robertocarvalho508@gmail.com>
2023-02-28 20:39:35 -0300

    aprendendo react

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
 
-function App() {
+
+function app(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div id='container'>
+        <div id='container-circle'></div>
+      </div>
+    </React.Fragment>
   );
-}
+};
 
-export default App;
+export default app;
---
+++ src/pages/home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+const Home: React.FC= () => {
+  return (
+    <React.Fragment>
+      <h1>Home</h1>
+      <p></p>
+    </React.Fragment>
+  );
+};
+
+export default Home;
